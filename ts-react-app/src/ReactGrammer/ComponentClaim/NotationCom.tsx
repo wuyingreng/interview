23888e655f817e38e38f63b23b70fc46bb3abd3c
peifang.xiang <peifang.xiang@lazada.com>
2024-01-17 22:37:50 +0800

    feat: add ts-react-app

---
+++ ts-react-app/src/ReactGrammer/ComponentClaim/NotationCom.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const Box = () => <div>Box</div>
+
+const BoxedFilters = () => {
+  return (
+    <div >
+      BoxedFilters
+    </div>
+  );
+};
+
+BoxedFilters.Box = Box;
+
+export default BoxedFilters;
---
+++ ts-react-app/src/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from './App';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+import "./index.css";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello world!</div>,
+//   },
+// ]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
