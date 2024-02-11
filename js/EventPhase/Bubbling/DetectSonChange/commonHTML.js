732d212184a7da5e131da55dab4916eda6b2738f
peifang.xiang <peifang.xiang@lazada.com>
2024-02-11 08:23:33 +0800

    feat: add bubble part

---
+++ js/EventPhase/Bubbling/DetectSonChange/commonHTML.js
@@ -0,0 +1,9 @@
+const commonHTML =`<table id="myTable">
+		<tr>
+			<td>Normal Cell</td>
+			<td id="specialCell">Special Cell</td>
+		</tr>
+		<!-- 其他行和单元格 -->
+	</table>`
+	
+document.body.innerHTML = commonHTML;
