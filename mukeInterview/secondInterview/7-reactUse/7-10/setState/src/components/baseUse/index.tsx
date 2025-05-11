bd05a80b46f27e820d28477a24d00b5f0d50227f
peifang.xiang <peifang.xiang@lazada.com>
2025-05-11 16:12:27 +0800

    feat:添加setState不可变值

---
+++ mukeInterview/secondInterview/7-reactUse/7-10/setState/src/components/baseUse/index.tsx
@@ -0,0 +1,29 @@
+// import JSXBaseDemo from './JSXBaseDemo'
+// import ConditionDemo from './ConditionDemo'
+// import ListDemo from './ListDemo'
+// import EventDemo from './EventDemo';
+
+// import FormDemo from './FormDemo'
+
+import StateDemo from './StateDemo'
+// import StateDemo1 from './StateDemo1'
+
+const BaseUseDemo = () => {
+    return <div>
+        {/* <JSXBaseDemo /> */}
+        {/* <ConditionDemo />
+            }
+        <EventDemo />
+        {/* <FormDemo/> */}
+        {/* <PropsDemo /> */}
+        <StateDemo />
+        {/* <StateDemo1/> */}
+
+    </div>
+
+}
+
+export default BaseUseDemo
+
+// React 组件生命周期图示
+// http://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
