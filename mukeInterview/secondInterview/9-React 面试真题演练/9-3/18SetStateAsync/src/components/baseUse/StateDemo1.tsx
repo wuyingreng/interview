44826a80d36d85c1996538987389a913c8e897bb
peifang.xiang <peifang.xiang@lazada.com>
2025-05-20 09:32:04 +0800

    feat:添加自定义Hooks

--- mukeInterview/secondInterview/9-React 面试真题演练/9-3/18SetStateAsync/src/components/baseUse/StateDemo1.tsx
+++ mukeInterview/secondInterview/9-React 面试真题演练/9-3/18SetStateAsync/src/components/baseUse/StateDemo1.tsx
@@ -12,18 +12,26 @@ const ListDemo = () => {
          * Todo:发现一个很奇怪的问题，每次点击保存这个文件，这个count会加1。
          * useEffect搭配空数组不是进入这个组件才执行一次吗
          * */
-        setCount(count + 1)
-        console.log('1', count) // 0
-        setCount(count + 1)
-        console.log('2', count) // 0
-        setTimeout(() => {
-            setCount(count + 1)
-            console.log('3', count) // 0
-        }, 500)
-        setTimeout(() => {
-            setCount(count + 1)
-            console.log('4', count) // 0
-        }, 500)
+        console.log('进入子组件');
+        setCount(count + 1);
+        setCount(count + 1);
+
+        // 保存定时器 ID
+        const timer1 = setTimeout(() => {
+            setCount(count + 1);
+            console.log('3', count);
+        }, 500);
+
+        const timer2 = setTimeout(() => {
+            setCount(count + 1);
+            console.log('4', count);
+        }, 500);
+
+        // 清理函数：组件卸载时清除定时器
+        return () => {
+            clearTimeout(timer1);
+            clearTimeout(timer2);
+        };
     }, [])
     return <p>{count}</p>
 }
