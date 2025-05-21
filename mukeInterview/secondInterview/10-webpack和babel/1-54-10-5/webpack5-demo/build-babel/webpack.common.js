b265e6f7ae2d0a191762b35dbce5c8d970044a04
peifang.xiang <peifang.xiang@lazada.com>
2025-05-21 11:57:07 +0800

    feat:添加webpack相关demo

---
+++ mukeInterview/secondInterview/10-webpack和babel/1-54-10-5/webpack5-demo/build-babel/webpack.common.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { srcPath, distPath } = require('./paths')
+const postcssPresetEnv = require('postcss-preset-env');
+module.exports = {
+    entry: path.join(srcPath, 'index'),
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader'],
+                include: srcPath,
+                // 因为node 包很多都是编译过后的，不需要再用babel-loader再处理一次
+                exclude: /node_modules/
+            },
+            // {
+            //     test: /\.css$/,
+            //     // loader 的执行顺序是：从后往前
+            //     use: ['style-loader', 'css-loader']
+            // },
+            {
+                test: /\.css$/,
+                // loader 的执行顺序是：从后往前
+                use: ['style-loader', 'css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: [postcssPresetEnv(/* pluginOptions */)]
+                        },
+                    },
+                }] // 加了 postcss
+            },
+            {
+                test: /\.less$/,
+                // 增加 'less-loader' ，注意顺序
+                use: ['style-loader', 'css-loader', 'less-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(srcPath, 'index.html'),
+            filename: 'index.html'
+        })
+    ]
+}
---
+++ mukeInterview/secondInterview/10-webpack和babel/1-54-10-5/webpack5-demo/src/index.js
@@ -0,0 +1,32 @@
+// 引入 css
+import './style/style1.css'
+import axios from 'axios'
+
+// import _ from 'lodash'
+// console.log(_.each)
+
+console.log('window.ENV', ENV)
+
+const print = (info) => {
+    console.log(info)
+}
+print('hello webpack 5')
+
+axios.get('/api2').then((res) => { }).catch(err => {
+    console.log('error')
+})
+import { sum } from './math'
+const sumRes = sum(10, 20)
+console.log('sumRes', sumRes)
+
+// 引入图片
+function insertImgElem(imgFile) {
+    const img = new Image()
+    img.src = imgFile
+    document.body.appendChild(img)
+}
+import imgFile1 from './img/1.png'
+insertImgElem(imgFile1)
+// import imgFile2 from './img/2.jpeg'
+insertImgElem(imgFile2)
+
