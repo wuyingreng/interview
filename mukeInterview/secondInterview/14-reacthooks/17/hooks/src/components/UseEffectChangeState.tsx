315daeea2b642767f54f0d749badd96a6fe9306e
peifang.xiang <peifang.xiang@lazada.com>
2025-05-22 15:31:44 +0800

    feat:useEffect下次执行前会执行上次的return函数

--- mukeInterview/secondInterview/14-reacthooks/17/hooks/src/components/UseEffectChangeState.tsx
+++ mukeInterview/secondInterview/14-reacthooks/17/hooks/src/components/UseEffectChangeState.tsx
@@ -1,32 +1,18 @@
-import React, { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 function UseEffectChangeState() {
     const [count, setCount] = useState(0)
-
-    /**
-     * useRef方案
-     * const countRef = useRef(0)
-     * setCount(++countRef.current)
-    */
-    // 模拟 DidMount
     const countRef = useRef(0)
-    console.log('countRef==>', countRef)
+
     useEffect(() => {
-        console.log('useEffect...', count)
-
-        // 定时任务
-        const timer = setInterval(() => {
-            console.log('setInterval...', count)
-            // setCount((preC) => preC + 1)
-            setCount(++countRef.current)
-        }, 1000)
-
-        // 清除定时任务
-        return () => clearTimeout(timer)
-    }, []) // 依赖为 []
+        setCount(++countRef.current)
+        return () => {
+            console.log('可以看到componentwillmount不停的执行')
+        }
+    }, [count,]) // 依赖为 []
 
     // 依赖为 [] 时： re-render 不会重新执行 effect 函数
-    // 没有依赖：re-render 会重新执行 effect 函数
+    // 没有依赖：re-render 会重新执行 effect 函数和return函数
 
     return <div>count: {count} </div>
 }
