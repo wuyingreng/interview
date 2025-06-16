649b0e8e61ef367929651b04143088d2de14d4a8
peifang.xiang <peifang.xiang@lazada.com>
2025-06-16 13:55:10 +0800

    feat: 添加virtual list

--- mukeInterview/一面/8[2reviewed]/8-17/asyncAwaitPromise.js
+++ mukeInterview/一面/8[2reviewed]/8-17/asyncAwaitPromise.js
@@ -1,18 +1,26 @@
 async function async1() {
-  console.log('async1 start')
+  console.log('async1 start') // 同步任务2
   await async2()
+  // 外层微任务
   console.log('async1 end')
 }
 
 async function async2() {
-  console.log('async2')
+  console.log('async2') // 同步任务3
+  await async3();
+  // 内层微任务
+  console.log('async2 end')
 }
 
-console.log('script start')
+async function async3() {
+  console.log('async3 end') // 同步任务4
+}
+
+console.log('script start') // 同步任务1
 
 
 setTimeout(() => {
-  console.log('setTimeout')
+  console.log('setTimeout') // 宏任务1
 }, 0);
 
 
@@ -20,10 +28,10 @@ async1()
 
 
 new Promise(function (resolve) {
-  console.log('promise 1')
+  console.log('promise 1') // 同步任务4
   resolve()
 }).then(() => {
-  console.log('promise 2')
+  console.log('promise 2') // 内层微任务2
 })
 
-console.log('script end')
+console.log('script end') // 同步任务5
