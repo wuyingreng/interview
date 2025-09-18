530bc475517c156f1faae4de7ebbbd04b3161f8a
peifang.xiang <peifang.xiang@lazada.com>
2025-09-18 16:18:08 +0800

    feat:用最新的插件

--- vue/vue-project/vite.config.ts
+++ vue/vue-project/vite.config.ts
@@ -2,15 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools';
-import VitePluginVueSetupExtend from 'vite-plugin-vue-setup-extend'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-    VitePluginVueSetupExtend()
+    vueDevTools()
   ],
   resolve: {
     alias: {
