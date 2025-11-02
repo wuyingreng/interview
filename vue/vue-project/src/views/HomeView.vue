970c3b68dbbb2ba53b03283e157b91226798bd60
peifang.xiang <peifang.xiang@lazada.com>
2025-11-02 11:16:09 +0800

    feat:添加模板语法

--- vue/vue-project/src/views/HomeView.vue
+++ vue/vue-project/src/views/HomeView.vue
@@ -1,15 +1,40 @@
-<script setup lang="ts">
-  import { ref } from 'vue'
-  import UseTemplateRef from '../components/UseTemplateRef.vue'
-  import Parent from '../components/Props/DefineProps/Parent.vue'
-  let show = ref(true)
-</script>
-
 <template>
   <main>
     <div>dom 相关</div>
     <UseTemplateRef />
     <div>props相关</div>
     <Parent v-if="show" />
+
+    <div>使用 JavaScript 表达式</div>
+    <div>{{ dogObject?.name }}</div>
+    <div v-bind="objAttrs">test obj attrs</div>
+    <div>指令 Directives</div>
+    <button :disabled="disabled">Button</button>
+    <button disabled="">测试disabled为 空字符串</button>
+
+    <a :[attrname]="href" target="_blank" @[enventName].prevent="log">跳转去百度</a>
+    <!-- <a :['foo' + bar]="value"> ... </a> -->
   </main>
 </template>
+<script setup>
+  import { ref } from 'vue'
+  import UseTemplateRef from '../components/UseTemplateRef.vue'
+  import Parent from '../components/Props/DefineProps/Parent.vue'
+  let show = ref(true)
+  let disabled = ref()
+  const dogObject = {
+    name: 'Emily',
+    age: 3,
+  }
+  const objAttrs = {
+    id: 'test',
+    class: 'wrapper',
+    style: 'background-color:red',
+  }
+  const attrname = 'href'
+  const href = 'https://www.baidu.com'
+  const enventName = 'click'
+  const log = () => {
+    console.log('a链接被点击了')
+  }
+</script>
